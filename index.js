df7255ed6527fc470f7dc52cb7fe4826c927c0e6
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-10-02 16:40:15 -0300

    Refactor index.js to add targetChannelId for channel configuration

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const client = new Client({
     ],
 });
 
+let targetChannelId = null;
+
 client.once('ready', () => {
     console.log(`¡Bot en línea!`);
     loadAgentes(); 
@@ -33,30 +35,38 @@ client.on('messageCreate', async message => {
     console.log('Mapa:', map);
     console.log('Rol:', rol);
 
-    if (message.content.startsWith(`${PREFIX}servidores`)) {
-        const guilds = client.guilds.cache.map(guild => guild.name).join('\n');
-        message.channel.send(`Estoy en los siguientes servidores:\n${guilds}`);
-        return;
+    if (message.content === '!channel here') {
+        targetChannelId = message.channel.id;
+        message.channel.send('Este canal ha sido configurado.');
     }
 
-    if (message.content.startsWith(`${PREFIX}roles`)) {
-        message.channel.send(`Roles disponibles: ${roles.join(', ')}`);
-        return;
-    }
-
-    if (message.content.startsWith(`${PREFIX}personajes`)) {
-        const personajesArray = Object.values(personajes).flat().map(agent => agent.displayName);
-        message.channel.send(`Personajes disponibles: ${personajesArray.join(', ')}`);
-        return;
-    }
-
-    if (message.content.startsWith(`${PREFIX}mapas`)) {
-        message.channel.send(`Mapas disponibles: ${Object.keys(mapas).join(', ')}`);
-        return;
-    }
-
-    if (message.content.startsWith(`${PREFIX}randhelp`) || message.content.startsWith(`${PREFIX}rhelp`)) {
-        const embed = new EmbedBuilder()
+    if (targetChannelId && message.channel.id === targetChannelId) {
+        
+        
+        if (message.content.startsWith(`${PREFIX}servidores`)) {
+            const guilds = client.guilds.cache.map(guild => guild.name).
+            message.channel.send(`Estoy en los siguientes servidores:\n${guilds}`);
+            return;
+        }
+        
+        if (message.content.startsWith(`${PREFIX}roles`)) {
+            message.channel.send(`Roles disponibles: ${roles.join(', ')}`);
+            return;
+        }
+        
+        if (message.content.startsWith(`${PREFIX}personajes`)) {
+            const personajesArray = Object.values(personajes).flat().map(agent => agent.displayName);
+            message.channel.send(`Personajes disponibles: ${personajesArray.join(', ')}`);
+            return;
+        }
+        
+        if (message.content.startsWith(`${PREFIX}mapas`)) {
+            message.channel.send(`Mapas disponibles: ${Object.keys(mapas).join(', ')}`);
+            return;
+        }
+        
+        if (message.content.startsWith(`${PREFIX}randhelp`) || message.content.startsWith(`${PREFIX}rhelp`)) {
+            const embed = new EmbedBuilder()
             .setColor('#0099ff')
             .setTitle('¡Comandos disponibles!')
             .setDescription(`¡Hola! Soy Randbot, técnicamente un Valorant Randomizer xd, te ayudaré a elegir un personaje aleatorio para jugar en Valorant. ¡Aquí tienes los comandos disponibles!`)
@@ -70,72 +80,72 @@ client.on('messageCreate', async message => {
                 { name: `${PREFIX}help`, value: `Muestra los comandos disponibles.` },
             )
             .setTimestamp();
-        
-        message.channel.send({ embeds: [embed] });
-        return;
-    }
-
-    if (message.content.startsWith(`${PREFIX}randomize`) || message.content.startsWith(`${PREFIX}r`)) {
-        const author = message.author;
-        const username = author.username; 
-        const avatarURL = author.displayAvatarURL(); 
-
-        const randomRole = roles[Math.floor(Math.random() * roles.length)]; 
-        const randomPersonaje = personajes[randomRole][Math.floor(Math.random() * personajes[randomRole].length)];
-
-        if (!map) {
-            const embed = new EmbedBuilder()
-            .setAuthor({ name: username, iconURL: avatarURL })
-            .setColor(randomPersonaje.backgroundGradientColors?.[0]?.replace(/ff$/, '') || '#0099ff') 
-            .setTitle(`¡${capitalizeFirstLetter(username)}, tu rol es: ${randomRole}!`) 
-            .setDescription(`Jugarás con: **${randomPersonaje.displayName}**`)
-            .setThumbnail(randomPersonaje.role.displayIcon)
-            .setImage(randomPersonaje.fullPortrait) 
-            .setTimestamp();
             
-            message.channel.send({ embeds: [embed] });   
+            message.channel.send({ embeds: [embed] });
             return;
         }
-
-        if (map) {
-            if (!mapas[capitalizeFirstLetter(map)]) {
-                message.channel.send(`El mapa "${map}" no es válido. Por favor, elige uno de los siguientes: ${Object.keys(mapas).join(', ')}`);
+        
+        if (message.content.startsWith(`${PREFIX}randomize`) || message.content.startsWith(`${PREFIX}r`)) {
+            const author = message.author;
+            const username = author.username; 
+            const avatarURL = author.displayAvatarURL(); 
+            
+            const randomRole = roles[Math.floor(Math.random() * roles.length)]; 
+            const randomPersonaje = personajes[randomRole][Math.floor(Math.random() * personajes[randomRole].length)];
+            
+            if (!map) {
+                const embed = new EmbedBuilder()
+                .setAuthor({ name: username, iconURL: avatarURL })
+                .setColor(randomPersonaje.backgroundGradientColors?.[0]?.replace(/ff$/, '') || '#0099ff') 
+                .setTitle(`¡${capitalizeFirstLetter(username)}, tu rol es: ${randomRole}!`) 
+                .setDescription(`Jugarás con: **${randomPersonaje.displayName}**`)
+                .setThumbnail(randomPersonaje.role.displayIcon)
+                .setImage(randomPersonaje.fullPortrait) 
+                .setTimestamp();
+                
+                message.channel.send({ embeds: [embed] });   
                 return;
             }
-
-            const personajesPorMapa = mapas[capitalizeFirstLetter(map)][randomRole]; 
-
-            if (!personajesPorMapa || personajesPorMapa.length === 0) {
-                message.channel.send(`No hay personajes disponibles para el rol "${randomRole}" en el mapa "${map}".`);
-                return;
-            }
-
-            if (rol) {
-                const capitalizedRol = capitalizeFirstLetter(rol);
-                rol = rolesMap[capitalizedRol] || rol;
-    
-                const personajesPorRol = personajes[capitalizeFirstLetter(rol)];
-                if (!personajesPorRol || personajesPorRol.length === 0) {
-                    message.channel.send(`No hay personajes disponibles para el rol "${rol}".`);
+            
+            if (map) {
+                if (!mapas[capitalizeFirstLetter(map)]) {
+                    message.channel.send(`El mapa "${map}" no es válido. Por favor, elige uno de los siguientes: ${Object.keys(mapas).join(', ')}`);
                     return;
                 }
                 
-                const personajesPorMapaRol = mapas[capitalizeFirstLetter(map)][capitalizeFirstLetter(rol)]; 
-                console.log(`Personajes para el mapa: ${map}: `, personajesPorMapaRol);
+                const personajesPorMapa = mapas[capitalizeFirstLetter(map)][randomRole]; 
                 
-                if (!personajesPorMapaRol || personajesPorMapaRol.length === 0) {
-                    message.channel.send(`No hay personajes disponibles para el rol "${rol}" en el mapa "${map}".`);
+                if (!personajesPorMapa || personajesPorMapa.length === 0) {
+                    message.channel.send(`No hay personajes disponibles para el rol "${randomRole}" en el mapa "${map}".`);
                     return;
                 }
                 
-                const randomPersonajeMapaRol = personajesPorMapaRol[Math.floor(Math.random() * personajesPorMapaRol.length)];
-                console.log(randomPersonajeMapaRol);
-
-                const PersonajeMapaRol = personajes[capitalizeFirstLetter(rol)].find(agent => agent.displayName === randomPersonajeMapaRol);
-                
-                const mapImage = await getMap(capitalizeFirstLetter(map))
-    
-                const rolEmbed = new EmbedBuilder()
+                if (rol) {
+                    const capitalizedRol = capitalizeFirstLetter(rol);
+                    rol = rolesMap[capitalizedRol] || rol;
+                    
+                    const personajesPorRol = personajes[capitalizeFirstLetter(rol)];
+                    if (!personajesPorRol || personajesPorRol.length === 0) {
+                        message.channel.send(`No hay personajes disponibles para el rol "${rol}".`);
+                        return;
+                    }
+                    
+                    const personajesPorMapaRol = mapas[capitalizeFirstLetter(map)][capitalizeFirstLetter(rol)]; 
+                    console.log(`Personajes para el mapa: ${map}: `, personajesPorMapaRol);
+                    
+                    if (!personajesPorMapaRol || personajesPorMapaRol.length === 0) {
+                        message.channel.send(`No hay personajes disponibles para el rol "${rol}" en el mapa "${map}".`);
+                        return;
+                    }
+                    
+                    const randomPersonajeMapaRol = personajesPorMapaRol[Math.floor(Math.random() * personajesPorMapaRol.length)];
+                    console.log(randomPersonajeMapaRol);
+                    
+                    const PersonajeMapaRol = personajes[capitalizeFirstLetter(rol)].find(agent => agent.displayName === randomPersonajeMapaRol);
+                    
+                    const mapImage = await getMap(capitalizeFirstLetter(map))
+                    
+                    const rolEmbed = new EmbedBuilder()
                     .setAuthor({ name: username, iconURL: avatarURL })
                     .setColor(PersonajeMapaRol.backgroundGradientColors?.[0]?.replace(/ff$/, '') || '#0099ff')
                     .setTitle(`¡${capitalizeFirstLetter(username)}, elegiste el rol: ${capitalizeFirstLetter(rol)}!`)
@@ -143,15 +153,15 @@ client.on('messageCreate', async message => {
                     .setThumbnail(PersonajeMapaRol.displayIcon)
                     .setImage(mapImage || 'default_image_url_here')
                     .setTimestamp();
-    
-                message.channel.send({ embeds: [rolEmbed] });
-                return;
-            }
-
-            const randomPersonajeMapName = personajesPorMapa[Math.floor(Math.random() * personajesPorMapa.length)];
-            const randomPersonajeMap = personajes[randomRole].find(agent => agent.displayName === randomPersonajeMapName);
-            const mapImage = await getMap(capitalizeFirstLetter(map))
-            const mapEmbed = new EmbedBuilder()
+                    
+                    message.channel.send({ embeds: [rolEmbed] });
+                    return;
+                }
+                
+                const randomPersonajeMapName = personajesPorMapa[Math.floor(Math.random() * personajesPorMapa.length)];
+                const randomPersonajeMap = personajes[randomRole].find(agent => agent.displayName === randomPersonajeMapName);
+                const mapImage = await getMap(capitalizeFirstLetter(map))
+                const mapEmbed = new EmbedBuilder()
                 .setAuthor({ name: username, iconURL: avatarURL })
                 .setColor(randomPersonajeMap.backgroundGradientColors?.[0]?.replace(/ff$/, '') || '#0099ff')
                 .setTitle(`¡${capitalizeFirstLetter(username)}, tu rol es: ${randomRole}!`)
@@ -159,14 +169,15 @@ client.on('messageCreate', async message => {
                 .setThumbnail(randomPersonajeMap.displayIcon)
                 .setImage(mapImage || 'default_image_url_here')
                 .setTimestamp();
-
-            message.channel.send({ embeds: [mapEmbed] });
-            return;
+                
+                message.channel.send({ embeds: [mapEmbed] });
+                return;
+            }
+            
         }
-
-        
-    }
-});
-
-
-client.login(process.env.BOT_TOKEN);
+            
+        }
+    });
+    
+    
+    client.login(process.env.BOT_TOKEN);
